d6dfd26c6cf78149e6ca37b2e791db7a19d1fe81
NaughtyPhoton <R3ndertim3>
2020-01-01 11:08:51 -0800

    Dockerizing app

--- python-API/app.py
+++ python-API/app.py
@@ -1,25 +1,36 @@
 from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
 
 from lookup.getLookup import get_lookup
 from src.content import Content
 from src.title import Title
 
 app = Flask(__name__)
+api = Api(app)
 
 lookup = get_lookup()
 
 
-@app.route('/get_title', methods=['get'])
-def get_title():
-    title = Title(lookup)
-    return jsonify(title.__dict__)
+# @app.route('/get_title', methods=['get'])
+# def get_title():
+#     title = Title(lookup)
+#     return jsonify(title.__dict__)
+#
+#
+# @app.route('/get_content', methods=['get'])
+# def get_content():
+#     yesterday_html = request.args.get('yesterday_html')
+#     content = Content(lookup, yesterday_html)
+#     return jsonify(content.__dict__)
 
 
-@app.route('/get_content', methods=['get'])
-def get_content():
-    yesterday_html = request.args.get('yesterday_html')
-    content = Content(lookup, yesterday_html)
-    return jsonify(content.__dict__)
+class GetTitle(Resource):
+    def get(self):
+        title = Title(lookup)
+        return jsonify(title.__dict__)
 
 
-app.run(port=5000)
+api.add_resource(GetTitle, "/get_title")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
