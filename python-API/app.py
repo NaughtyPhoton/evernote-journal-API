70684365e478983512c0a2245f2d8241d882eff8
NaughtyPhoton <R3ndertim3>
2020-01-01 15:56:04 -0800

    Did some things

--- python-API/app.py
+++ python-API/app.py
@@ -11,26 +11,21 @@ api = Api(app)
 lookup = get_lookup()
 
 
-# @app.route('/get_title', methods=['get'])
-# def get_title():
-#     title = Title(lookup)
-#     return jsonify(title.__dict__)
-#
-#
-# @app.route('/get_content', methods=['get'])
-# def get_content():
-#     yesterday_html = request.args.get('yesterday_html')
-#     content = Content(lookup, yesterday_html)
-#     return jsonify(content.__dict__)
-
-
 class GetTitle(Resource):
     def get(self):
         title = Title(lookup)
         return jsonify(title.__dict__)
 
 
+class GetContent(Resource):
+    def get(self):
+        yesterday_html = request.get_json().get('yesterday_html')
+        content = Content(lookup, yesterday_html)
+        return jsonify(content.__dict__)
+
+
 api.add_resource(GetTitle, "/get_title")
+api.add_resource(GetContent, "/get_content")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
